04e2130c1e445c7aa6dbd52bc02b7c8614a48fb4
nick <happilymarrieddad@gmail.com>
2023-04-24 16:22:00 -0600

    pushing images

--- deploys/push.sh
+++ deploys/push.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Using git to version control the images
+git add -A
+git commit -m "pushing images"
+git push origin master
+
 GIT_VERSION=$(git rev-parse HEAD)
 DOCKER_USERNAME="happilymarrieddadudemy"
 
 echo "Pushing Orderer Images"
 
-docker build -t ${DOCKER_USERNAME}/hyperledger-orderer-init-container:${GIT_VERSION} -f ./dockerfiles/Dockerfile.OrdererInitContainer .
+docker build -t ${DOCKER_USERNAME}/hyperledger-orderer-init-container:${GIT_VERSION} -f ../dockerfiles/Dockerfile.OrdererInitContainer .
 docker push ${DOCKER_USERNAME}/hyperledger-orderer-init-container:${GIT_VERSION}
+docker build -t ${DOCKER_USERNAME}/hyperledger-orderer-init-container:latest -f ../dockerfiles/Dockerfile.OrdererInitContainer .
+docker push ${DOCKER_USERNAME}/hyperledger-orderer-init-container:latest
