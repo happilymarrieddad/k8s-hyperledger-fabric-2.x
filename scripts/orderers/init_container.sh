e46f2efcd264367c98051cd19480c038330e46fa
nick <happilymarrieddad@gmail.com>
2023-04-24 16:41:34 -0600

    pushing images

--- scripts/orderers/init_container.sh
+++ scripts/orderers/init_container.sh
@@ -2,6 +2,7 @@
 
 /bin/sh -c > set -x;
 while [ $(curl -sw '%{http_code}' "http://${CA_HOST}:${CA_PORT}/cainfo" -o /dev/null) -ne 200 ]; do
+    echo "service 'http://${CA_HOST}:${CA_PORT}' not available so waiting 15 secs"
     sleep 15;
 done
 
