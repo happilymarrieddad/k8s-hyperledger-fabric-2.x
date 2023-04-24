453408c212fe3bfe92b1ce673396b9605f605a8d
nick <happilymarrieddad@gmail.com>
2023-04-24 16:22:33 -0600

    pushing images

--- deploys/push.sh
+++ deploys/push.sh
@@ -10,7 +10,7 @@ DOCKER_USERNAME="happilymarrieddadudemy"
 
 echo "Pushing Orderer Images"
 
-docker build -t ${DOCKER_USERNAME}/hyperledger-orderer-init-container:${GIT_VERSION} -f ../dockerfiles/Dockerfile.OrdererInitContainer .
+docker build -t ${DOCKER_USERNAME}/hyperledger-orderer-init-container:${GIT_VERSION} -f ./dockerfiles/Dockerfile.OrdererInitContainer .
 docker push ${DOCKER_USERNAME}/hyperledger-orderer-init-container:${GIT_VERSION}
-docker build -t ${DOCKER_USERNAME}/hyperledger-orderer-init-container:latest -f ../dockerfiles/Dockerfile.OrdererInitContainer .
+docker build -t ${DOCKER_USERNAME}/hyperledger-orderer-init-container:latest -f ./dockerfiles/Dockerfile.OrdererInitContainer .
 docker push ${DOCKER_USERNAME}/hyperledger-orderer-init-container:latest
