23f8066f91e224189675d0cdc2b6359b168d27ab
Nick Kotenberg <happilymarrieddad@gmail.com>
2023-04-25 14:35:19 -0600

    updated

--- scripts/orderers/init_container.sh
+++ scripts/orderers/init_container.sh
@@ -6,8 +6,8 @@ set -x;
 
 echo "Ready to start enroll process to get certs"
 
-# fabric-ca-client enroll \
-#     -u ${CA_SCHEME}://${CA_USERNAME}:${CA_PASSWORD}@${CA_URL} \
-#     --tls.certfiles /fabric-ca-server/tls-cert.pem
+fabric-ca-client enroll \
+    -u ${CA_SCHEME}://${CA_USERNAME}:${CA_PASSWORD}@${CA_URL} \
+    --tls.certfiles /fabric-ca-server/tls-cert.pem
 
 sleep infinity
